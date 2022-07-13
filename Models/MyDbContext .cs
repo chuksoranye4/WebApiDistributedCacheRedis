57bd433a36a14c51e03fa560784a4ed21cdc62f2
Chukwuka Oranye <Chukwuka.Oranye@ruhr-uni-bochum.de>
2022-07-13 23:28:37 +0200

    Add project files.

---
+++ Models/MyDbContext .cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApiDistributedCacheRedis.Models;
+public class MyDbContext : DbContext
+{
+    public MyDbContext(DbContextOptions<MyDbContext> options)
+           : base(options)
+    {
+    }
+    public DbSet<ArticleMatrix>? ArticleMatrices { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+using Microsoft.EntityFrameworkCore;
+using WebApiDistributedCacheRedis.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+ConfigurationManager configuration = builder.Configuration;
+// Add services to the container.
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<MyDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("ConnStr")));
+builder.Services.AddStackExchangeRedisCache(options => { options.Configuration = configuration["RedisCacheUrl"]; });
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
